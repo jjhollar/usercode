26ebc3633d8b1362c91d00521afd60aca9318a44
jjhollar <>
2008-10-20 12:29:41 +0000

    Allow for arbitrary paths to releases using scram list (for integration builds).

--- ConfdbDiffRecoConfigs/DoRecoDiff.sh
+++ ConfdbDiffRecoConfigs/DoRecoDiff.sh
@@ -11,11 +11,14 @@ fi
 
 
 CUR_DIR=`pwd`
-cd /afs/cern.ch/cms/sw/slc4_ia32_gcc345/cms/cmssw/$1/src
+THE_DIR=`scramv1 list | grep $1 | cut -f 2 -d ">" | grep afs` 
+cd $THE_DIR
 eval `scramv1 runtime -sh`
 cd $CUR_DIR
 $CUR_DIR/ConfdbMakeOfflineConfig.py >& CMSSWold_cff.py
 eval `scramv1 runtime -sh`
 export THE_OLD_RELEASE_NAME=$1
 $CUR_DIR/ConfdbDiffOfflineConfig.py >& thediff.twikiformat.txt
+rm CMSSWold_cff.py
+rm CMSSWold_cff.pyc
 exit
