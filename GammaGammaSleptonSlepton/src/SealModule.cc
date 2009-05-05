5dde341a2d33b4c48e64f5f3ad67c2769ce4a435
jjhollar <>
2009-05-05 12:12:47 +0000

    Remove obsolete code.

--- GammaGammaSleptonSlepton/src/SealModule.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "UserCode/GammaGammaSleptonSlepton/interface/GammaGammaSleptonSlepton.h"
-#include "UserCode/GammaGammaSleptonSlepton/interface/GenGammaGammaSleptonSlepton.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_FWK_MODULE(GammaGammaSleptonSlepton);
-DEFINE_FWK_MODULE(GenGammaGammaSleptonSlepton);
